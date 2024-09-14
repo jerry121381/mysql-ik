d92c9fe472a88651f0ba8bd53adc3ca12c588cb3
胡淞杰 <husj@lvxinsiyuan.com>
2024-09-14 17:03:09 +0800

    init

---
+++ src/main/java/com/lxsy/MySQLIkApplication.java
@@ -0,0 +1,15 @@
+package com.lxsy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: mysql-ik application starter
+ * @author: husj
+ * @create: 2024-09-13
+ **/
+@SpringBootApplication
+public class MySQLIkApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
